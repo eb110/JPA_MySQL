dd7dd25d7a37788eede3b13f9af299231e659f6d
eb110 <wfigura@op.pl>
2021-11-02 08:26:06 +0000

    remove test from main branch

--- src/main/java/com/JPA/MySQL/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.JPA.MySQL.repository;
-
-import com.JPA.MySQL.model.Test;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestRepository extends CrudRepository<Test, Integer> {
-}
