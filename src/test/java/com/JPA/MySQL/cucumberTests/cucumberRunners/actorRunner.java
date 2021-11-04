5872647f8829c704cede1dc14ab0c80b9efa32f2
eb110 <wfigura@op.pl>
2021-11-04 17:36:31 +0000

    test branch pom update

--- src/test/java/com/JPA/MySQL/cucumberTests/cucumberRunners/actorRunner.java
+++ src/test/java/com/JPA/MySQL/cucumberTests/cucumberRunners/actorRunner.java
@@ -1,13 +1,13 @@
 package com.JPA.MySQL.cucumberTests.cucumberRunners;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith( SpringRunner.class )
+@RunWith( Cucumber.class )
 @CucumberOptions(
         features = "src/test/resources/cucumberFeatures/actor.feature",
         glue = {"com.JPA.MySQL.cucumberTests.cucumberSteps"}
--- src/test/java/com/JPA/MySQL/cucumberTests/cucumberSteps/ActorSteps.java
+++ src/test/java/com/JPA/MySQL/cucumberTests/cucumberSteps/ActorSteps.java
@@ -2,14 +2,15 @@ package com.JPA.MySQL.cucumberTests.cucumberSteps;
 
 import com.JPA.MySQL.model.Actor;
 import com.JPA.MySQL.service.ActorServiceImpl;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.picocontainer.annotations.Inject;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class ActorSteps {
 
     @Autowired
