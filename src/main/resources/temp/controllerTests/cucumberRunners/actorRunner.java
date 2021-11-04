e1aef3433cb502cfdca3873f5904694945c147b7
eb110 <wfigura@op.pl>
2021-11-04 09:59:19 +0000

    unit tests test

--- src/main/resources/temp/controllerTests/cucumberRunners/actorRunner.java
+++ src/main/resources/temp/controllerTests/cucumberRunners/actorRunner.java
@@ -3,7 +3,6 @@ package com.JPA.MySQL.cucumberTests.cucumberRunners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 @RunWith( Cucumber.class )
