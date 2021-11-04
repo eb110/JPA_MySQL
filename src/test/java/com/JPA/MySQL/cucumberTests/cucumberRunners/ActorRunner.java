5fa4f5b970f4b0a4ddb6b20eec35b3dbe51a101e
eb110 <wfigura@op.pl>
2021-11-04 16:43:01 +0000

    app refactoring

--- src/test/java/com/JPA/MySQL/cucumberTests/cucumberRunners/ActorRunner.java
+++ src/test/java/com/JPA/MySQL/cucumberTests/cucumberRunners/ActorRunner.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 @RunWith( Cucumber.class )
+
 @CucumberOptions(
         features = "src/test/resources/cucumberFeatures/actor.feature",
         glue = {"com.JPA.MySQL.cucumberTests.cucumberSteps"}
