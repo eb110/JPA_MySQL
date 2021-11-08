3c5a67de155f557a808841b2e632f9dfaa910af7
eb110 <wfigura@op.pl>
2021-11-08 16:47:16 +0000

    film lombok remove

--- src/main/java/com/JPA/MySQL/model/VladFilm.java
+++ src/main/java/com/JPA/MySQL/model/VladFilm.java
@@ -9,10 +9,6 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Entity
 public class VladFilm {
 
@@ -30,4 +26,47 @@ public class VladFilm {
     @OneToMany(targetEntity = VladFilmActors.class, cascade = CascadeType.ALL)
     @JoinColumn(name = "idFilm", referencedColumnName = "id")
     private List<VladFilmActors> filmActorsList = new ArrayList<>();
+
+    public VladFilm() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIdDirector() {
+        return idDirector;
+    }
+
+    public void setIdDirector(int idDirector) {
+        this.idDirector = idDirector;
+    }
+
+    public List<VladFilmFeedback> getFilmFeedbackList() {
+        return filmFeedbackList;
+    }
+
+    public void setFilmFeedbackList(List<VladFilmFeedback> filmFeedbackList) {
+        this.filmFeedbackList = filmFeedbackList;
+    }
+
+    public List<VladFilmActors> getFilmActorsList() {
+        return filmActorsList;
+    }
+
+    public void setFilmActorsList(List<VladFilmActors> filmActorsList) {
+        this.filmActorsList = filmActorsList;
+    }
 }
