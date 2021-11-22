c0ccd9e5e48c4e8e623f7dafe79fea3c22a84ae7
eb110 <wfigura@op.pl>
2021-11-22 17:27:49 +0000

    cors 3000 open

--- src/main/java/com/JPA/MySQL/controller/ActorController.java
+++ src/main/java/com/JPA/MySQL/controller/ActorController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/actor")
+
 public class ActorController {
     @Autowired
     private ActorService actorService;
--- src/main/java/com/JPA/MySQL/controller/DirectorFeedbackController.java
+++ src/main/java/com/JPA/MySQL/controller/DirectorFeedbackController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/directorFeedback")
 public class DirectorFeedbackController {
     @Autowired
