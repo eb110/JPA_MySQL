272a27796c6417d4b5779dabbc680edae19f9dbd
eb110 <wfigura@op.pl>
2021-12-01 17:18:29 +0000

    selenium

--- src/test/java/com/JPA/MySQL/Sel002Test.java
+++ src/test/java/com/JPA/MySQL/Sel002Test.java
@@ -1,3 +1,5 @@
+package com.JPA.MySQL;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.Before;
@@ -7,6 +9,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -33,8 +36,13 @@ public class Sel002Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
+   // WebDriverManager.chromedriver().setup();
+   // driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("headless");
+    options.addArguments("disable-gpu");
+    driver = new ChromeDriver(options);
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
@@ -45,17 +53,29 @@ public class Sel002Test {
   @Test
   public void sel002() {
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    driver.get("http://localhost:3000/");
-    driver.manage().window().setSize(new Dimension(1536, 824));
-    driver.findElement(By.cssSelector(".movieButton:nth-child(2)")).click();
-    driver.findElement(By.cssSelector(".movieButton:nth-child(4)")).click();
+    driver.get("http://54.211.251.198:3000");
+    driver.manage().window().setSize(new Dimension(1920, 882));
     driver.findElement(By.cssSelector(".movieButton:nth-child(3)")).click();
+    driver.findElement(By.cssSelector(".movieButton:nth-child(5)")).click();
+    driver.findElement(By.cssSelector(".movieButton:nth-child(4)")).click();
     driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackAddFeedbackButton")).click();
     driver.findElement(By.cssSelector(".inputActorFeedback")).click();
-    driver.findElement(By.cssSelector(".inputActorFeedback")).sendKeys("new feedback");
+    driver.findElement(By.cssSelector(".inputActorFeedback")).sendKeys("feedb 01");
+    driver.findElement(By.cssSelector(".actorFeedbackAddButton")).click();
     driver.findElement(By.cssSelector(".actorFeedbackAddButton")).click();
     driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackButtonShow")).click();
-    driver.findElement(By.cssSelector("div:nth-child(1) > div > .actorFeedDeleteButton")).click();
-    driver.close();
+    {
+      WebElement element = driver.findElement(By.cssSelector("div:nth-child(1) > div > .btn-outline-warning"));
+      Actions builder = new Actions(driver);
+      builder.moveToElement(element).perform();
+    }
+    driver.findElement(By.cssSelector("div:nth-child(3) > div > .actorFeedDeleteButton")).click();
+    driver.findElement(By.cssSelector("div:nth-child(2) > div > .btn-outline-warning")).click();
+    driver.findElement(By.cssSelector(".updateActorFeedback")).click();
+    driver.findElement(By.cssSelector(".updateActorFeedback")).sendKeys("this is update");
+    driver.findElement(By.cssSelector(".updateFeedbackAddButton")).click();
+    driver.findElement(By.cssSelector(".updateActorFeedback")).click();
+    driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackAddFeedbackButton")).click();
+    driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackButtonShow")).click();
   }
 }
