ec252d976ff3a3ee46d7b5ee2b5c860a24f932a7
eb110 <wfigura@op.pl>
2021-12-02 12:21:36 +0000

    selenium

--- src/test/java/com/JPA/MySQL/Sel002Test.java
+++ src/test/java/com/JPA/MySQL/Sel002Test.java
@@ -1,3 +1,5 @@
+package com.JPA.MySQL;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.Before;
@@ -49,31 +51,42 @@ public class Sel002Test {
     driver.quit();
   }
   @Test
-  public void sel002() {
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    driver.get("http://54.211.251.198:3000");
-    driver.manage().window().setSize(new Dimension(1920, 882));
+  public void sel002() throws InterruptedException {
+
+  // driver.get("http://54.211.251.198:3000");
+    driver.get("http://localhost:3000/");
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.manage().window().setSize(new Dimension(1536, 824));
     driver.findElement(By.cssSelector(".movieButton:nth-child(3)")).click();
-    driver.findElement(By.cssSelector(".movieButton:nth-child(5)")).click();
-    driver.findElement(By.cssSelector(".movieButton:nth-child(4)")).click();
-    driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackAddFeedbackButton")).click();
-    driver.findElement(By.cssSelector(".inputActorFeedback")).click();
-    driver.findElement(By.cssSelector(".inputActorFeedback")).sendKeys("feedb 01");
-    driver.findElement(By.cssSelector(".actorFeedbackAddButton")).click();
-    driver.findElement(By.cssSelector(".actorFeedbackAddButton")).click();
-    driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackButtonShow")).click();
-    {
-      WebElement element = driver.findElement(By.cssSelector("div:nth-child(1) > div > .btn-outline-warning"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).perform();
-    }
-    driver.findElement(By.cssSelector("div:nth-child(3) > div > .actorFeedDeleteButton")).click();
-    driver.findElement(By.cssSelector("div:nth-child(2) > div > .btn-outline-warning")).click();
-    driver.findElement(By.cssSelector(".updateActorFeedback")).click();
-    driver.findElement(By.cssSelector(".updateActorFeedback")).sendKeys("this is update");
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".filmTodo:nth-child(1) .filmFeedbackButtonShow")).click();
+    TimeUnit.SECONDS.sleep(1);
+    js.executeScript("window.scrollTo(0,213.81817626953125)");
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".filmTodo:nth-child(1) .filmFeedbackAddFeedbackButton")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".inputFilmFeedback")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".inputFilmFeedback")).sendKeys("new fedb");
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".filmFeedbackAddButton")).click();
+    TimeUnit.SECONDS.sleep(1);
+    js.executeScript("window.scrollTo(0,225.4545440673828)");
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector("div:nth-child(1) > div > .btn-outline-warning")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".updateFilmFeedback")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".updateFilmFeedback")).sendKeys("nice one test");
+    TimeUnit.SECONDS.sleep(1);
     driver.findElement(By.cssSelector(".updateFeedbackAddButton")).click();
-    driver.findElement(By.cssSelector(".updateActorFeedback")).click();
-    driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackAddFeedbackButton")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector("div:nth-child(2) > div > .filmFeedDeleteButton")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.findElement(By.cssSelector(".movieButton:nth-child(4)")).click();
+    TimeUnit.SECONDS.sleep(1);
     driver.findElement(By.cssSelector(".actorTodo:nth-child(1) .actorFeedbackButtonShow")).click();
+    TimeUnit.SECONDS.sleep(1);
+    driver.close();
   }
 }
